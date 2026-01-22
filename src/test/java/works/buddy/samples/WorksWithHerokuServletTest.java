d47c04d7e92d8b59d2dc897fa80487b48fffcc36
Miltos717 <miltos717@gmail.com>
2026-01-22 12:50:16 +0200

    c

--- src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java
+++ src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java
@@ -11,7 +11,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintWriter;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
-        
+         
 public class WorksWithHerokuServletTest {
     
     private WorksWithHerokuServlet servlet;
